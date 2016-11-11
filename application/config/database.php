ad8653e532e4fa56d074f60dd4b19aaa90991f35
Viktor Tunggul <xu.redzonez@gmail.com>
2016-11-11 14:29:21 +0700

    no message

--- application/config/database.php
+++ application/config/database.php
@@ -5,7 +5,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "";
+$db['default']['password'] = "toor212";
 $db['default']['database'] = "epus_antrian_3671";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
