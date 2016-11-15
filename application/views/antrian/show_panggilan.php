b4198fc1fc073d30e4dabb595714aa537f422f81
Viktor Tunggul <xu.redzonez@gmail.com>
2016-11-15 09:03:51 +0700

    no message

--- application/views/antrian/show_panggilan.php
+++ application/views/antrian/show_panggilan.php
@@ -15,7 +15,7 @@
 	function playmp3(no,playlist){
 		var audioElement = document.createElement('audio');
 		if(playlist[no]!= undefined && playlist[no]!= ''){
-		    audioElement.setAttribute('src', "<?php echo base_url()?>/public/sound/" + playlist[no] + ".mp3");
+		    audioElement.setAttribute('src', "http://localhost/antrian/public/sound/" + playlist[no] + ".mp3");
 	        audioElement.setAttribute('autoplay', 'autoplay');
 	        audioElement.play();
 		}
