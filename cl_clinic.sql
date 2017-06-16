7325b2093a1b25a877d07519451efcc91a806605
Viktor Tunggul <xu.redzonez@gmail.com>
2017-06-16 10:33:35 +0700

    update clinic

---
+++ cl_clinic.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `cl_clinic` (`id`, `kode`, `value`, `type`, `jenis`, `kode_mapping`, `keyword`, `is_antrian`, `is_daftar`) VALUES
+(51, 'IMUNBCG', 'IMUNISASI BCG', 'RJ', 'sakit', '001', 'IMUNBCG', 1, 1),
+(53, 'IMUNPOLIO', 'IMUNISASI POLIO', 'RJ', 'sakit', '001', 'IMUNPOLIO', 1, 1),
+(54, 'IMUNCAMPAK', 'IMUNISASI CAMPAK', 'RJ', 'sakit', '001', 'IMUNCAMPAK', 1, 1),
+(55, 'IMUNHEP', 'IMUNISASI HEPATITIS', 'RJ', 'sakit', '001', 'IMUNHEP', 1, 1),
+(52, 'IMUNDPT', 'IMUNISASI DPT', 'RJ', 'sakit', '001', 'IMUNDPT', 1, 1);
+
